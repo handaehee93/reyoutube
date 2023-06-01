c89593e06e843c7bf03dddf4d79b5d55b394d6bd
handaehee93 <hdh8990@naver.com>
2023-06-01 12:48:50 +0900

    commit

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import VideoCard from '../components/VideoCard'
+import Youtube, { search } from '../api/youtube'
+import testYoutube from '../api/testYoutubeClient'
+import { useYoutubeApi } from '../context/YouTubeApiContext'
+
+
+export default function Videos() {
+  const { keyword } = useParams()
+  const {youtube} = useYoutubeApi()
+  const { data: videos } = useQuery(['videos',keyword], () => youtube.search(keyword), {staleTime: 1000 * 60 })
+
+  return (
+    <>
+      {videos &&
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4 '>
+          {videos.map(video => <VideoCard key={video.id} video={video} />)}
+        </ul>
+      }
+    </>
+  )
+}
